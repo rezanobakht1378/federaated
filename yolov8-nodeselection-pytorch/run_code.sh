f0065f0b1c09ed5f7d9eba278a30293da2b46540
root <a.mkh000@gmail.com>
2026-01-29 12:36:54 +0000

    Added: local-simulation-gpu to use gpu and cpue resource

--- yolov8-nodeselection-pytorch/run_code.sh
+++ yolov8-nodeselection-pytorch/run_code.sh
@@ -1,2 +1,2 @@
 #flwr run . 2>&1 | tee result.txt debug.log
-flwr run . 2>&1 | tee results/run_\$(date +"%Y%m%d_%H%M%S").log
+flwr run . local-simulation-gpu  2>&1 | tee results/run_\$(date +"%Y%m%d_%H%M%S").log
