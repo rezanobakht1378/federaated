307440e51f5411ca6e7653a23247b768bb416b3c
root <a.mkh000@gmail.com>
2026-01-29 12:41:11 +0000

    Changed: run_code to save logs based on date

--- yolov8-nodeselection-pytorch/run_code.sh
+++ yolov8-nodeselection-pytorch/run_code.sh
@@ -1,2 +1,6 @@
 #flwr run . 2>&1 | tee result.txt debug.log
-flwr run . local-simulation-gpu  2>&1 | tee results/run_\$(date +"%Y%m%d_%H%M%S").log
+#flwr run . local-simulation-gpu  2>&1 | tee results/run_\$(date +"%Y%m%d_%H%M%S").log
+
+#!/bin/bash
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+flwr run . local-simulation-gpu  2>&1 | tee "results/run_${TIMESTAMP}.log"
